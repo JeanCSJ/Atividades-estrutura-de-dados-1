d9530ad65d38d660c193fa8c9b37a83205caae2d
Jean Carlos Serafim Júnior <126007564+JeanCSJ@users.noreply.github.com>
2025-05-07 13:25:25 -0300

    Add files via upload

---
+++ Atividades_Estrutura_de_Dados/src/atividade4/Main.java
@@ -0,0 +1,34 @@
+package atividade4;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Stack<Character> pilha = new Stack<>();
+		
+		System.out.println("Digite o texto a ser modificado:");
+		String texto = sc.nextLine();
+		System.out.println("\nResultado:");
+		
+		for(int posicao = 0; posicao < texto.length(); posicao++) {
+			if(texto.charAt(posicao) != ' ') {
+				pilha.push(texto.charAt(posicao));
+			}else {
+				while(pilha.empty() == false){
+					System.out.print(pilha.pop());
+				}
+				System.out.print(' ');
+			}
+		}
+		while(pilha.empty() == false){
+			System.out.print(pilha.pop());
+		}
+		
+		
+		sc.close();
+	}
+
+}
